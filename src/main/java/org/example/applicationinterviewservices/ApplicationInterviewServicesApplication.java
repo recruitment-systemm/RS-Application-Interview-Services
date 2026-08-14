2d6fffba4db906ac5293d2299aefc3acc84bc555
ahmedali109 <ahmedalinaguib33@gmail.com>
2026-08-15 01:48:13 +0300

    chore: Setup Spring Boot Application

---
+++ src/main/java/org/example/applicationinterviewservices/ApplicationInterviewServicesApplication.java
@@ -0,0 +1,13 @@
+package org.example.applicationinterviewservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationInterviewServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationInterviewServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/applicationinterviewservices/ApplicationInterviewServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.applicationinterviewservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationInterviewServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
